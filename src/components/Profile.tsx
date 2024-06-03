83a4b032b986ee0d9c2ad0686e6b92da15b8e936
leejunghyeok <wndgur76@naver.com>
2024-06-03 14:36:02 +0900

    home profile items

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from "react";
 import "./Profile.css";
+import Tag from "./Tag";
+import ProfileItem from "./ProfileItem";
 
 interface ProfileProps {
 
@@ -8,24 +10,43 @@ interface ProfileProps {
 const Profile: FunctionComponent<ProfileProps> = () => {
     return (
         <div className="profile">
-            <div className="profile-img-wrapper">
-                <img className="profile-img" src="/images/profile.jpeg" alt="profile" />
+            <div className="left">
+                <ProfileItem>
+                    <h2>CONTACT</h2>
+                    <p>010-1234-5678</p>
+                </ProfileItem>
             </div>
-            <div className="profile-content">
-                <h1 className="profile-name">이중혁</h1>
-                <p className="profile-description">
-                    프론트엔드 개발자입니다.
-                </p>
-                <div className="profile-links">
-                    <a href="https://github.com" className="profile-link">Github</a>
+            <div className="center">
+                <div className="profile-img-wrapper">
+                    <img className="profile-img" src="/images/profile.jpeg" alt="profile" />
                 </div>
-                <div className="profile-tags">
-                    <span className="tag">React</span>
-                    <span className="tag">TypeScript</span>
-                    <span className="tag">JavaScript</span>
-                    <span className="tag">HTML</span>
-                    <span className="tag">CSS</span>
+                <div className="profile-content">
+                    <h1 className="profile-name">이중혁</h1>
+                    <p className="profile-description">
+                        협력, 성장!
+                    </p>
+                    <div className="profile-links">
+                        <a href="https://github.com" className="profile-link">Github</a>
+                    </div>
+                    <div className="profile-tags">
+                        <Tag tag="React" />
+                        <Tag tag="TypeScript" />
+                        <Tag tag="HTML" />
+                        <Tag tag="CSS" />
+                    </div>
                 </div>
+            </div>
+            <div className="right">
+                <ProfileItem>
+                    <h2>한경국립대학교</h2>
+                    <h3>컴퓨터공학과</h3>
+                    <span>2018.03 ~ 2024.02</span>
+                </ProfileItem>
+                <ProfileItem>
+                    <h2>한경국립대학교</h2>
+                    <h3>컴퓨터공학과</h3>
+                    <span>2018.03 ~ 2024.02</span>
+                </ProfileItem>
             </div>
         </div>
     );
