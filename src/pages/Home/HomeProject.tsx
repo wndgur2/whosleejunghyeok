5b7fb9cfa67838ef2c375ee2d24b515f49e9ff46
leejunghyeok <wndgur76@naver.com>
2024-06-12 19:15:07 +0900

    flexible projects list

--- src/pages/Home/HomeProject.tsx
+++ src/pages/Home/HomeProject.tsx
@@ -3,18 +3,22 @@ import _Post from "../../types/_Post";
 import Tag from "../../components/Tag";
 import './HomeProject.css';
 import { Link } from "react-router-dom";
+import parse from 'html-react-parser';
+import { CiImageOff } from "react-icons/ci";
 
 interface HomeProjectProps {
     post: _Post;
-    image: string;
 }
 
-const HomeProject: FunctionComponent<HomeProjectProps> = ({ post, image }) => {
+const HomeProject: FunctionComponent<HomeProjectProps> = ({ post }) => {
+    const thumbnail = post.content.match(/<img[^>]+src="([^">]+)".*>/);
     return (
         <article className="project">
             <Link className="project-info link" to={`/post/${post.title}`}>
                 <div className="project-image">
-                    <img src={image} alt={post.title} />
+                    {thumbnail ? parse(thumbnail[0] + "</img>") :
+                        <div><CiImageOff size={32} /></div>
+                    }
                 </div>
                 <h3 className="project-title">{post.title}</h3>
                 <p className="project-discription">{post.description}</p>
