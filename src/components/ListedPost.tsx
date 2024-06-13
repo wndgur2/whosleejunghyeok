4366e44efb8046d3b94954ea3d1be81fe7dd92b7
leejunghyeok <wndgur76@naver.com>
2024-06-13 16:12:53 +0900

    listed post gets post as prop.

--- src/components/ListedPost.tsx
+++ src/components/ListedPost.tsx
@@ -3,24 +3,27 @@ import './ListedPost.css';
 import Tag from "./Tag";
 import { BsCaretRightFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import _Post from "../types/_Post";
 
 interface ListedPostProps {
-    title: string;
-    tags: string[];
+    post: _Post;
 }
 
-const ListedPost: FunctionComponent<ListedPostProps> = ({ title, tags }: ListedPostProps) => {
+const ListedPost: FunctionComponent<ListedPostProps> = ({ post }: ListedPostProps) => {
     return (
         <article className="listed-post">
-            <Link className="listed-post-title link" to={`/post/${title}`}>
-                <BsCaretRightFill className="vertical-bar" size={14} />
-                <h3>{title}</h3>
+            <Link className="listed-post-title link" to={`/post/${post.title}`}>
+                <h3>
+                    <BsCaretRightFill className="triangle" size={14} />
+                    <span>{post.title}</span>
+                </h3>
+                <small>{post.date_started}~ {post.date_finished}</small>
             </Link>
-            <div className="tags">
-                {tags.map((tag, index) => (
+            <ol className="tags">
+                {post.tags.map((tag, index) => (
                     <Tag key={index} tag={tag} />
                 ))}
-            </div>
+            </ol>
         </article>
     );
 }
