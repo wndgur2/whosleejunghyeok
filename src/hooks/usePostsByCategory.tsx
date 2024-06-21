3d6ddb97d7eab2944d3be65188f550f54dfd69c3
leejunghyeok <wndgur76@naver.com>
2024-06-21 17:50:56 +0900

    home post limit

--- src/hooks/usePostsByCategory.tsx
+++ src/hooks/usePostsByCategory.tsx
@@ -6,6 +6,7 @@ const usePostsByCategory = (posts: _Post[]) => {
         const _postsByCategory: any = {};
         posts.forEach(post => {
             if (!_postsByCategory[post.category]) _postsByCategory[post.category] = [];
+            else if (_postsByCategory[post.category].length > 10) return;
             _postsByCategory[post.category].push(post);
         });
         return _postsByCategory;
