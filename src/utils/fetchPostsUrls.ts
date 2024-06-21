56d69f6c773cab36e3cbc4b66f0dd38ed3419fe9
leejunghyeok <wndgur76@naver.com>
2024-06-21 14:01:00 +0900

    faster post insert

--- src/utils/fetchPostsUrls.ts
+++ src/utils/fetchPostsUrls.ts
@@ -1,6 +1,6 @@
 const fetchPostUrls = async()=>{
     try{
-        const response = await fetch('https://raw.githubusercontent.com/wndgur2/wndgur2.github.io/main/posts/urls.json');
+        const response = await fetch('https://raw.githubusercontent.com/wndgur2/wndgur2.github.io/main/scripts/urls.json');
         const data = await response.json();
         return data;
     } catch(err){
--- src/utils/postsReducer.ts
+++ src/utils/postsReducer.ts
@@ -8,11 +8,15 @@ const reducer = (state: _Post[], action: PostAction): _Post[] => {
             // if post already exists, return state
             if(state.find((prevPost:_Post) => prevPost.id === action.payload.id)) return state;
             // insert post sorted by date_started
-            // find index
-            let index = 0;
-            for(let i=0; i<state.length; i++){
-                if(state[i].date_started > action.payload.date_started) index = i;
-                else break;
+            // find index by binary search
+            let left = 0;
+            let right = state.length - 1;
+            let index;
+            while (left <= right) {
+                index = Math.floor((left + right) / 2);
+                if (state[index].date_started === action.payload.date_started) break;
+                if (state[index].date_started < action.payload.date_started) right = index - 1;
+                else left = index + 1;
             }
             return [...state.slice(0, index), action.payload, ...state.slice(index)];
 
